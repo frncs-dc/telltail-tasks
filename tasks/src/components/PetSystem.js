072d22843d09077cbff16c931f52310e8b622fc2
Frances <97236341+frncs-dc@users.noreply.github.com>
2024-01-18 19:29:03 +0800

    Update PetSystem.js

---
+++ tasks/src/components/PetSystem.js
@@ -0,0 +1,25 @@
+import HomeTerrain from '../components/HomeTerrain';
+import UserControls from '../components/UserControls';
+//import '../../public/index.css';
+import {useState} from "react";
+
+
+function PetSystem() {
+  
+  const [selectedCommand, setSelectedCommand] = useState(null);
+
+  const handleSelectCommand = (userCommand) => {
+    setSelectedCommand(userCommand);
+  }
+
+  return (
+    <div>
+      <h1>Normal Terrain</h1>
+      <div>Currently doing: {selectedCommand}</div>
+      <HomeTerrain command = {selectedCommand} />
+      <UserControls onSelectCommand={handleSelectCommand} />
+    </div>
+  );
+}
+
+export default PetSystem;
